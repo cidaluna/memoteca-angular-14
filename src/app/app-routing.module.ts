d43651a84f9ca3549c50a73f338f93f3918eb50d
cidaluna <cidalunna@hotmail.com>
2023-04-16 10:54:47 -0300

    Created listar pensamento component and config routes on app routing module ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // cada rota é um objeto contendo o path endereço url e o nome do componente que deseja exibir na tela
+  {
+    path:'', pathMatch: 'full', redirectTo: 'listarPensamento'
+  },
+  {
+    path: 'criarPensamento', component: CriarPensamentoComponent
+  },
+  {
+    path: 'listarPensamento', component: ListarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
