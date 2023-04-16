1b9b3324900195d29110b756a8968546fb3d758a
cidaluna <cidalunna@hotmail.com>
2023-04-16 12:03:42 -0300

    Sharing data between child and parent directives

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
+  // Este componente pai passa as informações para o componente filho
+  listaPensamentos = [
+    {
+    conteudo: 'Aprendendo Angular com Cida Luna',
+    autoria: 'Dev Cida',
+    modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Teste objeto declarado no componente pai',
+      autoria: 'Componente pai',
+      modelo: 'modelo1'
+      }
+  ];
 
   constructor() { }
 
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pensamento.component.css']
 })
 export class PensamentoComponent implements OnInit {
+ /* Este é o componente filho, ele deve receber as informações do componente pai,
+  *  que é o listar-pensamento, para isso deve utilizar o decorator @Input()
+  */
 
-  pensamento = {
+ @Input() pensamento = {
     conteudo: 'I love Angular',
     autoria: 'Cida Luna',
     modelo: 'modelo3'
