5ff0f560f62fe89893695d747cddedd3ce0251d0
cidaluna <cidalunna@hotmail.com>
2023-04-16 11:40:34 -0300

    Thought card list created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { FormsModule } from '@angular/forms';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
-import { PensamentoComponent } from './componenetes/pensamentos/pensamento/pensamento.component';
+import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
 
 @NgModule({
   declarations: [
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
+  pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Cida Luna',
+    modelo: 'modelo3'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
